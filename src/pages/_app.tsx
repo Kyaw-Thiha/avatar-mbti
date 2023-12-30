ef951ecce3cff6fb6a6ddb0d906366b355d5f921
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-12-30 13:00:28 +0630

    Added shadcn components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
+import { AnimatePresence } from "framer-motion";
 
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Component key={router.route} {...pageProps} />
+    </AnimatePresence>
+  );
 };
 
 export default api.withTRPC(MyApp);
