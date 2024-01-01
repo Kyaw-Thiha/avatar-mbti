63a3baeb967d58dbdcf2eaf69045154b5ba3f6cd
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-01-01 20:53:06 +0630

    Added ENFJ personality

--- src/data/enfj.ts
+++ src/data/enfj.ts
@@ -1,17 +1,23 @@
 export const enfjData = {
   name: "Spider",
-  image: "jake-sully.webp",
+  image: "spider.webp",
   personality: {
     code: "ENFJ",
     name: "Protagonist",
-    descriptionImage: "jake-sully-2.webp",
+    descriptionImage: "spider-2.webp",
     description: `Protagonists tend to be vocal about their values, including authenticity and altruism. When something strikes them as unjust or wrong, they speak up. But they rarely come across as brash or pushy, as their sensitivity and insight guide them to speak in ways that resonate with others.
 
 These personality types have an uncanny ability to pick up on people’s underlying motivations and beliefs. At times, they may not even understand how they come to grasp another person’s mind and heart so quickly. These flashes of insight can make Protagonists incredibly persuasive and inspiring communicators.
     
 Changing people’s minds is no easy task – but if anyone can do it, it’s Protagonists.
 Protagonists’ secret weapon is their purity of intent. Generally speaking, they are motivated by a sincere wish to do the right thing rather than a desire to manipulate or have power over other people. Even when they disagree with someone, Protagonists search for common ground. The result is that people with this personality type can communicate with an eloquence and sensitivity that are nearly impossible to ignore – particularly when they speak about matters that are close to their hearts.`,
-    characterImage: "jake-sully-3.jpg",
-    characterLink: ``,
+    characterImage: "spider.webp",
+    characterLink: `One example of Spider displaying ENFJ traits is his relationship with Quaritch. Despite Quaritch being the leader of the Recombinants, he identifies Spider as his son and attempts to gain his favor. This inclination towards familial ties and working harmoniously with others is characteristic of an ENFJ's loyal and nurturing nature.
+
+Moreover, Spider's decision to teach Quaritch about Na'vi culture and language illustrates his empathetic side, another ENFJ trait. He chooses to educate rather than retaliate, showing his ability to emotionally connect with others and his tendency to accommodate and educate for mutual understanding.
+    
+Importantly, the ENFJ personality type is also known for their strong intuitive and judgment-based decision-making. Spider's actions during the encounter with the Metkayina and the humans underscore this. He cripples the vessel, allowing for the rescue of his friends but also getting his own father's reluctant recognition. Not only does this show his diplomatic skills characteristic of an ENFJ, but it also shows his willingness to make tough calls for the greater good, a part of the 'Judging' aspect of his personality.
+    
+Lastly, Spider's decision to refuse Quaritch's offer to leave with him and instead rejoin Jake's family showcases his ENFJ tendency towards loyalty and a strong sense of responsibility to others. His final choice reveals his commitment to maintaining harmony and the well-being of the group, further cementing his ENFJ personality traits.`,
   },
 };
